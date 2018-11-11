2b3eb5ee25cd561e1a6a1a93d124421f43f26e6b
TheViking <rodrigotonifreitas@gmail.com>
2018-11-11 19:03:55 -0200

    ExercicioIII

---
+++ exercicioIII/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = "Gabriela";
+$sexo = "Feminino";
+$idade = 23;
+
+if($sexo == 'Feminino' && $idade < 25)
+{
+    echo "{$nome} Aceita";
+}
+else
+{
+    echo "{$nome} Não aceita";
+}
